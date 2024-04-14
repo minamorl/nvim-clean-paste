2692fa2842cb7e41f9584437f1b2d251491785ff
minamorl <minamorl@users.noreply.github.com>
2024-04-14 19:39:22 +0900

    fix for clipboard

--- lua/nvim-clean-paste/init.lua
+++ lua/nvim-clean-paste/init.lua
@@ -1,10 +1,14 @@
 local M = {}
 
 function M.custom_paste()
-    local yanked_text = vim.fn.getreg('"')
+    local clipboard_reg = '*'
+    if vim.fn.has('unnamedplus') == 1 then
+        clipboard_reg = '+'
+    end
+    local yanked_text = vim.fn.getreg(clipboard_reg)
     local cleaned_text = string.gsub(yanked_text, "\r", "")
-    vim.fn.setreg('"', cleaned_text)
-    vim.cmd('normal! ""p')
+    vim.fn.setreg(clipboard_reg, cleaned_text)
+    vim.cmd('normal! "' .. clipboard_reg .. 'p')
 end
 
 return M
