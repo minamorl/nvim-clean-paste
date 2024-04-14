f853eca87a49111ea49598741b90ada4e0da3105
minamorl <minamorl@users.noreply.github.com>
2024-04-14 19:27:23 +0900

    simple way

--- lua/nvim-clean-paste/init.lua
+++ lua/nvim-clean-paste/init.lua
@@ -1,24 +1,10 @@
 local M = {}
 
-function M.setup()
-    -- Set up the autocmd for TextYankPost event
-    vim.api.nvim_create_autocmd("TextYankPost", {
-        callback = function()
-            vim.schedule(function()
-                -- Get the contents of the unnamed register
-                local content = vim.fn.getreg('"')
-
-                -- Remove ^M characters from the content
-                content = content:gsub('\r', '')
-
-                -- Set the modified content back to the unnamed register
-                vim.fn.setreg('"', content)
-
-                -- Perform the default paste operation
-                vim.cmd('normal! p')
-            end)
-        end,
-    })
+function M.custom_paste()
+    local yanked_text = vim.fn.getreg('"')
+    local cleaned_text = string.gsub(yanked_text, "\r", "")
+    vim.fn.setreg('"', cleaned_text)
+    vim.cmd('normal! ""p')
 end
 
 return M
